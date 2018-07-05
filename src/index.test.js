b6cc94d80869e05590d4b5832c3c11cd4994f552
sulen <magicznagrzywka@gmail.com>
2018-07-05 14:04:36 +0200

    added example tests

---
+++ src/index.test.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import jsdom from 'jsdom';
+const { JSDOM } = jsdom
+
+describe('example test', () => {
+  it('should pass', () => {
+    expect(true).toEqual(true);
+  });
+});
+
+describe('index.html', () => {
+  it('should say hello', () => {
+    const index = fs.readFileSync('./src/index.html', 'utf-8');
+    const dom = new JSDOM(index);
+
+    const h1 = dom.window.document.getElementsByTagName('h1')[0];
+    expect(h1.innerHTML).toEqual('Hello World');
+    window.close();
+
+  });
+});
